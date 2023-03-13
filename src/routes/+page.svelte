2b3e13a3e8184801b7d1117da88094c531dc94ba
Rubén Chiquin <randreu28@gmail.com>
2023-03-13 18:44:54 +0100

    Adds tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-screen justify-center items-center">
+	<h1>Hello world!</h1>
+</div>
