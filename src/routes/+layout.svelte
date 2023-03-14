275d8a3bb65f4747cd5360ac8f9e4fa8cd4f7750
Rubén Chiquin <randreu28@gmail.com>
2023-03-14 09:43:05 +0100

    Adds a github button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '../app.css';
+	import 'iconify-icon';
 </script>
 
+<a href="https://github.com/randreu28/learning-svelte" target="_blank">
+	<iconify-icon icon="mdi:github" class="text-5xl absolute right-5 top-5" />
+</a>
 <slot />
